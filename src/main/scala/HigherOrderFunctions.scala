deef9499fce533ae77738ad6dc560fd7ca18ab75
abhishyam <chabhishyam@gmail.com>
2017-06-04 11:17:36 +0530

    Anonymous functions in Scala

--- src/main/scala/HigherOrderFunctions.scala
+++ src/main/scala/HigherOrderFunctions.scala
@@ -8,18 +8,40 @@
   */
 object HigherOrderFunctions {
   def main(args: Array[String]) {
-    function1(5)
-  }
-
-  def function1(a:Int ): Unit ={
-    println(a)
-    println(multiplyBy2(a))
+    function1(5.345, multiplyBy2)
   }
 
   /**
+    * Function which accepts other function
+    * as parameter
+    * @param a
+    * @param f
+    */
+  def function1(a:Double , f: Double => AnyVal){
+    println(a)
+    println(f(a))
+  }
+
+  /**
+    * This is functions in which we have some
+    * anonymous functions.
+    * An Anonymous function can be create in 2 ways
+    * 1). => (rocket)
+    * 2)_ (underscore)
+    */
+  def anonymousFunctions(): Unit ={
+    //anonymous function with rocket symbol
+      var result = (a:Int, b:Int) => {
+        val x = a * 2
+        val y = b * 2
+        (x+y)/2
+      }
+    println(result)
+  }
+  /**
     * function to multiply a number by 2
     */
-  def multiplyBy2(a: Int) = {
+  def multiplyBy2(a: Double) = {
     a*2
   }
 }
